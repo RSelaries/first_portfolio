a79c6d8a0ba0f9341a822de9eebdd2c70abeee0b
RSelaries <raphael.selaries@ensaama.net>
2023-12-28 22:20:07 +0100

    changed background blur for home page

--- assets/js-assets/scrollPercentage.js
+++ assets/js-assets/scrollPercentage.js
@@ -8,10 +8,11 @@ $(document).ready(function() {
         let scrollPercentRounded = Math.round(scrollPercent*100);
 
         let about = document.querySelector('.for-blur')
-        let blur = Math.round(scrollPercent * 10)
+        let blur = Math.round(scrollPercent * 15)
         blur -= 3
         if (blur < 0) { blur = 0 }
         about.style.backdropFilter = `blur(${blur}px)`
-        about.style.backgroundColor = `#777${blur}`
+        let blur2 = blur * 0.2
+        about.style.backgroundColor = `#999${blur2}`
     }
 )});
