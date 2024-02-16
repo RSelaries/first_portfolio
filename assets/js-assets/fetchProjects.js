0ee7e6506d18384758fae5d378d5f39159e00d0b
RSelaries <raphael.selaries@ensaama.net>
2024-02-16 13:25:21 +0100

    changed some images

--- assets/js-assets/fetchProjects.js
+++ assets/js-assets/fetchProjects.js
@@ -5,19 +5,34 @@ async function fetchProjects(pageName) {
     const response = await window.fetch(url);
     const content = await response.text();
     const doc = parser.parseFromString(content, "application/xhtml+xml");
-    console.log(doc);
+    // console.log(doc);
 
     const projects = doc.querySelectorAll("div.forProjectPage");
-    console.log(projects);
+    // console.log(projects);
 
     for (const project of projects) {
-        const projectName = project.id
+        const projectName = project.id;
 
         $(`#${projectName}`).load(
             `../${pageName}/projects/${projectName}.html`
         );
     }
 }
+
+const projects = document.querySelectorAll(".forProjectPage");
+
+for (let i = 0; i < projects.length; i++) {
+    let project = projects[i];
+
+    project.onmouseover = function (e) {
+        project.classList.add("hover");
+    };
+
+    project.onmouseleave = function (e) {
+        project.classList.remove("hover");
+    };
+}
+
 
 Object.assign(window, {
     fetchProjects,
